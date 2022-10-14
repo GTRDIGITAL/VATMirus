007701e09d0cda0fbe751a69939c6b672500e451
GTRDIGITAL <GTRDigital@ro.gt.com>
2022-10-14 09:19:58 +0300

    modified d394.xml saving

--- app.py
+++ app.py
@@ -4685,7 +4685,9 @@ def D300():
 		# f=open("C:/Users/Bogdan.Constantinesc/Documents/D300 to XML Final CI/D300 to XML 2/storage/D394.xml", "w",encoding='utf-8').write(text)
 		# make_archive("C:/Users/Bogdan.Constantinesc/Documents/D300 to XML Final CI/D300 to XML 2/storage","C:/Users/Bogdan.Constantinesc/Documents/D300 to XML Final CI/D300 to XML 2/arhiva VAT apps.zip")
 		# return send_from_directory("C:/Users/Bogdan.Constantinesc/Documents/D300 to XML Final CI/D300 to XML 2","arhiva VAT apps.zip",as_attachment=True)
-		f=open("/home/mirus_app/storage/D394.xml", "w",encoding='utf-8').write(text)
+		# f=open("/home/mirus_app/storage/D394.xml", "w",encoding='utf-8').write(text)
+		with open("/home/mirus_app/storage/D394.xml", "w", encoding="utf-8") as g:
+			g.write(text)
 	make_archive("/home/mirus_app/storage","/home/mirus_app/storage/arhiva_VAT_apps.zip")
 	return send_from_directory("/home/mirus_app/storage","arhiva_VAT_apps.zip",as_attachment=True)
 @app.route('/CUI_Extractor')
