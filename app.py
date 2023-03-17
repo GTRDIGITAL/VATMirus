bff4d893711c33292c5c558631a7055318a63f46
cnstba <bogdan.constantinescu@ro.gt.com>
2023-03-17 12:43:34 +0200

    changesPMG

--- app.py
+++ app.py
@@ -2064,7 +2064,7 @@ IF(AND('Cover sheet'!D47<>"nil",'Cover sheet'!D43="Yes"),'Cover sheet'!D47+IFERR
             print(taxcodeach)
             a=3
             for x in range(0, len(taxcodeach)):
-                if(lunacurenta[x]=="Yes"):
+                if(lunacurenta[x]=="Yes" and vata[x]!=None):
                     if str(taxcodeach[x])=="C3" or str(taxcodeach[x])=="E1" :
                         a=a+1
                         workings.cell(row=a, column=1).value="A"
@@ -2080,7 +2080,7 @@ IF(AND('Cover sheet'!D47<>"nil",'Cover sheet'!D43="Yes"),'Cover sheet'!D47+IFERR
 
 
             for x in range(0, len(taxcodeach)):
-                if(lunacurenta[x]=="Yes"):              
+                if(lunacurenta[x]=="Yes" and vata[x]!=None):              
                     if str(taxcodeach[x])=="X1":
                         a=a+1
                         workings.cell(row=a, column=1).value="S"
@@ -2095,7 +2095,7 @@ IF(AND('Cover sheet'!D47<>"nil",'Cover sheet'!D43="Yes"),'Cover sheet'!D47+IFERR
                         workings.cell(row=a, column=9).value='=IF(F{0}=" "," ",COUNTIFS(F{0}:F10000,F{0},A{0}:A10000,A{0}))'.format(a)
 
             for x in range(0, len(taxcodes)):
-                if(listacurentas[x]=="Yes"):                
+                if(listacurentas[x]=="Yes" and vats[x]!=None):                
                     if str(taxcodes[x])=="Y3" or str(taxcodes[x])=="Y1":
                         a=a+1
                         workings.cell(row=a, column=1).value="L"
@@ -2109,7 +2109,7 @@ IF(AND('Cover sheet'!D47<>"nil",'Cover sheet'!D43="Yes"),'Cover sheet'!D47+IFERR
                         workings.cell(row=a, column=5).value='=ROUND(H{0},0)'.format(a)
                         workings.cell(row=a, column=9).value='=IF(F{0}=" "," ",COUNTIFS(F{0}:F10000,F{0},A{0}:A10000,A{0}))'.format(a)
             for x in range(0, len(taxcodes)):
-                if(listacurentas[x]=="Yes"):                
+                if(listacurentas[x]=="Yes" and and vats[x]!=None):                
                     if str(taxcodes[x])=="Y4":
                         a=a+1
                         workings.cell(row=a, column=1).value="P"
